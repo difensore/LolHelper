b8897f478f483f456c2630370de896112f52fd40
difensore <aledima0209@gmail.com>
2022-07-07 21:50:19 +0300

    SomeFixAdded

--- SpellPrint.cs
+++ SpellPrint.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,21 +11,32 @@ namespace LolHelper
     {
         public async Task Asyncprint(int id)
         {
-           await using (LolHelperContext db = new LolHelperContext())
+            await using (LolHelperContext db = new LolHelperContext())
             {
-                var FirstSpell = from Spell in db.Spells
-                                join Pick in db.Picks on Spell.Id equals Pick.FirstSpell
-                                where Pick.Id == id
-                                select Spell;
-                var SecondSpell = from Spell in db.Spells
-                                 join Pick in db.Picks on Spell.Id equals Pick.SecondSpell
+                var FirstSpell =await(from Spell in db.Spells
+                                 join Pick in db.Picks on Spell.Id equals Pick.FirstSpell
                                  where Pick.Id == id
-                                 select Spell;
+                                 select Spell).ToListAsync();
+              /*  var SecondSpell = from Spell in db.Spells
+                                  join Pick in db.Picks on Spell.Id equals Pick.SecondSpell
+                                  where Pick.Id == id
+                                  select Spell;*/
                 Console.WriteLine("\nSpells:");
                 foreach (var s in FirstSpell)
                 {
                     Console.WriteLine($"First spell: {s.Name}, cooldowm-{s.Cooldown} seconds");
                 }
+             /*   foreach (var s in SecondSpell)
+                {
+                    Console.WriteLine($"Second spell: {s.Name}, cooldowm-{s.Cooldown} seconds");
+                }*/
+            }
+            await using(LolHelperContext db = new LolHelperContext())
+            {
+                var SecondSpell = await(from Spell in db.Spells
+                                  join Pick in db.Picks on Spell.Id equals Pick.SecondSpell
+                                  where Pick.Id == id
+                                  select Spell).ToListAsync();
                 foreach (var s in SecondSpell)
                 {
                     Console.WriteLine($"Second spell: {s.Name}, cooldowm-{s.Cooldown} seconds");
